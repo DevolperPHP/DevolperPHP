be643ecd26e6a101063d7834d47a11da3fb7773d
DevolperPHP <devolperphp@gmail.com>
2016-09-05 17:20:53 +0300

    Update GhastSound.php

--- src/sound/GhastSound.php
+++ src/sound/GhastSound.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace GhastSound;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\level\sound\GhastSound;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+class GhastSound extends PluginBase{
+  
+  public function onEnable(){
+    $this->getServer()->getLogger()->info(Color::GREEN."Plugin is on")
+  }
+  
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    switch($cmd->getName()){
+      case 'sound':
+        $sender->getLevel()->addSound(new GhastSound($sender));
+    }
+  }
+}
