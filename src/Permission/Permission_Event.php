a7243ee5cc17a88259aa1dbe88d587bb5ac43c31
DevolperPHP <devolperphp@gmail.com>
2016-09-05 18:04:35 +0300

    Update Permission_Event.php

--- src/Permission/Permission_Event.php
+++ src/Permission/Permission_Event.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace Permission_Event;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\permission\permission;
+use pocketmine\utils\TextFormat as Color;
+
+class Permission_Event extends PluginBase implements Listener{
+  
+  public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	
+	public function onJoin(PlayerJoinEvent $event){
+	  $player = $event->getPlayer();
+	  $name = $player->getName();
+	  
+	  if($player->hasPermission("permission.join")){
+	    $player->setJoinMessage(Color::GREEN."$name Join The Server !!!");
+	  } else {
+	    $player->setJoinMessage(Color::YELLOW."$name Join The Server");
+	  }
+	}
+}
