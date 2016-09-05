7f1193c6f8601dedc219de1bd4254be303c73053
DevolperPHP <devolperphp@gmail.com>
2016-09-05 17:33:54 +0300

    Update Op_Command.php

--- src/Op/Op_Command.php
+++ src/Op/Op_Command.php
@@ -1,1 +1,23 @@
+<?php
 
+namespace Op_Command;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as Color;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+class AnvilUseSound extends PluginBase{
+  
+  public function onEnable(){
+    $this->getServer()->getLogger()->info(Color::GREEN."Plugin is on")
+  }
+  
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    if($sender->isOp()){
+      switch($cmd->getName()){
+        case 'cmd':
+          $sender->sendMessage(Color::WHITE."Hi");
+      }
+    }
+  }
+}
