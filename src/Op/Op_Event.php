fbf9667303b37725f6ab8848a1d09672489ddf10
DevolperPHP <devolperphp@gmail.com>
2016-09-05 17:40:27 +0300

    Update Op_Event.php

--- src/Op/Op_Event.php
+++ src/Op/Op_Event.php
@@ -1,3 +1,23 @@
 <?php
 
 namespace Op_Event;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\level\particle\HeartParticle;
+
+class Op_Event extends PluginBase implements Listener(){
+  
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  
+  public function onMove(PlayerMoveEvent $event){
+    $player = $event->getPlayer();
+    
+    if($player->isOp()){
+      $player->getLevel()->addParticle(new HeartParticle($player));
+    }
+  }
+}
