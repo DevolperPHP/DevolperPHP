2e06c46f7a8546a57d9bc11ee114914602c3e233
DevolperPHP <devolperphp@gmail.com>
2016-09-05 17:44:52 +0300

    Update Permission_Command.php

--- src/Permission/Permission_Command.php
+++ src/Permission/Permission_Command.php
@@ -1,1 +1,9 @@
+<?php
 
+namespace Permission_Command;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\permission\Permission;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\utils\TextFormat as Color;
