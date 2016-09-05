0d2e0b87c83a5c3092d82291e28a5aeaed4e6b75
DevolperPHP <devolperphp@gmail.com>
2016-09-05 18:21:49 +0300

    Update Hunger.php

--- src/Hunger.php
+++ src/Hunger.php
@@ -1,1 +1,21 @@
+<?php
 
+namespace Hunger;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+class Hunger extends PluginBase{
+  
+  public function onEnable(){
+    $this->getServer()->getLogger()->info(Color::GREEN."Plugin Is On");
+  }
+  
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    switch($cmd->getName()){
+      case 'food':
+        $sender->setFood(20);
+    }
+  }
+}
