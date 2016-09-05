647e499f75f4403adb0e4a68c3bf4f7095ae4ef7
DevolperPHP <devolperphp@gmail.com>
2016-09-05 15:20:04 +0300

    Update DataPacketReceiveEvent.php

--- src/event/DataPacketReceiveEvent.php
+++ src/event/DataPacketReceiveEvent.php
@@ -1,1 +1,25 @@
+<?php
 
+namespace DataPackReceiveEvent;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\item\Item;
+use pocketmine\utils\TextFormat as Color;
+
+class DataPacketReceiveEvent extends PluginBase implements Listener{
+  
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  
+  public function onPackReceiveEvent(DataPackReceiveEvent $event){
+    $player = $event->getPlayer();
+    $item = $player->getInventory()->getItemInHand()->getId();
+    
+    if($item == id){
+      $player->sendTip("Hellow");
+    }
+  }
+}
