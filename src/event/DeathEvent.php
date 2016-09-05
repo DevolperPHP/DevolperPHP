a2345ebb8db82aad76290ba4f023fb5d9db3e133
DevolperPHP <devolperphp@gmail.com>
2016-09-05 15:26:47 +0300

    Update DeathEvent.php

--- src/event/DeathEvent.php
+++ src/event/DeathEvent.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace DeathEvent;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\utils\TextFormat as Color;
+
+class DeathEvent extends PluginBase implements Listener{
+  
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  
+  public function onDeath(PlayerDeathEvent $event){
+    $player = $event->getPlayer();
+    $name = $player->getName();
+    
+    $player->setDeathMessage(Color::RED."$name did");
+  }
+}
