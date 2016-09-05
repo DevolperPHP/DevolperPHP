442e8ffbbb9be2efcdadb67b001ba8699133d3dc
DevolperPHP <devolperphp@gmail.com>
2016-09-05 18:20:29 +0300

    Update ExpLevel.php

--- src/ExpLevel.php
+++ src/ExpLevel.php
@@ -1,1 +1,21 @@
+<?php
 
+namespace ExpLevel;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+class ExpLevel extends PluginBase{
+  
+  public function onEnable(){
+    $this->getServer()->getLogger()->info(Color::GREEN."Plugin Is On");
+  }
+  
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    switch($cmd->getName()){
+      case 'level':
+        $sender->setExpLevel(99999);
+    }
+  }
+}
