bf3925feccdeec93c7d204e2fe29d7ea2a5f17b1
DevolperPHP <devolperphp@gmail.com>
2016-09-05 14:34:03 +0300

    Update HeartParticle.php

--- src/Particle/HeartParticle.php
+++ src/Particle/HeartParticle.php
@@ -1,1 +1,23 @@
+<?php
 
+namespace HeartParticle;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\level\particle\HeartParticle;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\utils\TextFormat as Color;
+
+class HeartParticle extends PluginBase{
+  
+  public function onEnable(){
+    $this->getServer()->getLogger()->info(Color::GREEN."Plugin is on");
+  }
+  
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    switch($cmd->getName()){
+      case 'particle':
+        $sender->getLevel()->addParticle(new HeartParticle($sender));
+    }
+  }
+}
