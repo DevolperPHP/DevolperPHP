e278d076e0e609c9c9a1647192be8e857e12201e
DevolperPHP <devolperphp@gmail.com>
2016-09-05 16:54:47 +0300

    Update SignChangeEvent.php

--- src/event/SignChangeEvent.php
+++ src/event/SignChangeEvent.php
@@ -1,1 +1,26 @@
+<?php
 
+namespace SignChangeEvent;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\block\SignChangeEvent;
+use pocketmine\block\Block;
+use pocketmine\utils\TextFormat as Color;
+
+class SignChangeEvent extends PluginBase implements Listener{
+  
+  public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	
+	public function onSignChange(SignChangeEvent){
+	  $sign = $event->setLine();
+	  $player = $event->getPlayer();
+	  
+	  $sign(0,"Hi");
+	  $sign(1,"$event->getName()");
+	  $sign(2,"Enjoy =)");
+	  
+	  $player->sedMessage("You Touch The Sign");
+	}
+}
