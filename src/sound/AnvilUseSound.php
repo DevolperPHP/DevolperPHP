cfe35edfb39789ba6554143fe6f20b90a3928a89
DevolperPHP <devolperphp@gmail.com>
2016-09-05 17:15:58 +0300

    Update AnvilUseSound.php

--- src/sound/AnvilUseSound.php
+++ src/sound/AnvilUseSound.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace AnvilUseSound;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\level\sound\AvilUseSound;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+class AnvilUseSound extends PluginBase{
+  
+  public function onEnable(){
+    $this->getServer()->getLogger()->info(Color::GREEN."Plugin is on")
+  }
+  
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    switch($cmd->getName()){
+      case 'sound':
+        $sender->getLevel()->addSound(new AnvilUseSound($sender));
+    }
+  }
+}
