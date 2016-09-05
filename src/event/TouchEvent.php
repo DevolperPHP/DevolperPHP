4e501104f9903bd015985b709bd676a74642392d
DevolperPHP <devolperphp@gmail.com>
2016-09-05 17:02:26 +0300

    Update TouchEvent.php

--- src/event/TouchEvent.php
+++ src/event/TouchEvent.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace TouchEvent;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\block\Block;
+use pocketmine\utils\TextFormat;
+
+class TouchEvent extends PluginBase implements Listener{
+  
+  public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	
+	public function onInteract(PlayerInteractEvent $event){
+	  $player = $event->getPlayer();
+	  $block = $event->getBlock()->getId();
+	  $name = $player->getName();
+	  
+	  if($block == id){
+	    
+	    $player->sendMessage(Color::GREEN."You Touch The Block");
+	  }
+	}
+}
