314dc254a40f7969de802b2e0d25ce7f8cda74ef
DevolperPHP <devolperphp@gmail.com>
2016-09-05 15:51:16 +0300

    Update JoinEvent.php

--- src/event/JoinEvent.php
+++ src/event/JoinEvent.php
@@ -1,1 +1,15 @@
+<?php
 
+namespace JoinEvent;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\utils\TextFormat as Color;
+
+class JoinEvent extends PluginBase implements Listener{
+  
+  public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+}
