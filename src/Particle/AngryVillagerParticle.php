40885f3631c314649706a62ebe64e5f386e1bf36
DevolperPHP <devolperphp@gmail.com>
2016-09-05 14:14:33 +0300

    Update AngryVillagerParticle.php

--- src/Particle/AngryVillagerParticle.php
+++ src/Particle/AngryVillagerParticle.php
@@ -1,1 +1,20 @@
+<?php
 
+use pocketmine\plugin\PluginBase;
+use pocketmine\level\particle\AngryVillagerParticle;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+class AngryVillagerParticle extends PluginBase{
+  
+  public function onEnable(){
+    $this->getServer()->getLogger()->info(Color::GREEN."Plugin is on");
+  }
+  
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    switch($cmd->getName()){
+      case 'particle':
+        $sender->getLevel()->addParticle(new AngryVillagerParticle($sender))
+    }
+  }
+}
