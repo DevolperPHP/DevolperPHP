5ebb96d94730faadc871982ecd28f23f27a45eea
DevolperPHP <devolperphp@gmail.com>
2016-09-05 16:26:01 +0300

    Update MoveEvent.php

--- src/event/MoveEvent.php
+++ src/event/MoveEvent.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace MoveEvent;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\utils\TextFormat as Color;
+
+class MoveEvent extends PluginBase implements Listener{
+  
+  public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	
+	public function onMove(PlayerMoveEvent $event){
+	  $player = $event->getPlayer();
+	  $name = $player->getName();
+	  
+	  $player->sendPopup(Color::AQUA."Welcome $name");
+	}
+}
