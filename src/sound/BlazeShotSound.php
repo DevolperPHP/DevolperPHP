7589c77806c3b5324baa7f927f71788cbd97ce42
DevolperPHP <devolperphp@gmail.com>
2016-09-05 17:17:59 +0300

    Update BlazeShotSound.php

--- src/sound/BlazeShotSound.php
+++ src/sound/BlazeShotSound.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace BlazeShotSound;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\level\sound\BlazeShotSound;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+class BlazeShotSound extends PluginBase{
+  
+  public function onEnable(){
+    $this->getServer()->getLogger()->info(Color::GREEN."Plugin is on")
+  }
+  
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    switch($cmd->getName()){
+      case 'sound':
+        $sender->getLevel()->addSound(new BlazeShotSound($sender));
+    }
+  }
+}
