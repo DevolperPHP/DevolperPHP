9699ed7c5e1a6b6c3ed744eef2f5c250be86d3e5
DevolperPHP <devolperphp@gmail.com>
2016-09-05 15:34:24 +0300

    Update DropItemEvent.php

--- src/event/DropItemEvent.php
+++ src/event/DropItemEvent.php
@@ -1,1 +1,24 @@
+<?php
 
+namespace DropItemEvent;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDropItem;
+use pocketmine\utils\TextFormat as Color;
+
+class DropItemEvent extends PluginBase implements Listener{
+  
+  public function onEnabel(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  
+  public function onItemDrop(PlayerDropItemEvent $event){
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        $item = $event->getItem()->getId();
+        
+        if($item == id){
+          $player->sendMessage(Color::GREEN."Hi");
+        }
+}
