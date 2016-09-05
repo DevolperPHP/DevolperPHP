005b97f2f3b5c6b4c6fc62ad4bb8cbf923a64bbe
DevolperPHP <devolperphp@gmail.com>
2016-09-05 17:19:22 +0300

    Update ClickSound.php

--- src/sound/ClickSound.php
+++ src/sound/ClickSound.php
@@ -1,1 +1,21 @@
+<?php
+namespace ClickSound;
 
+use pocketmine\plugin\PluginBase;
+use pocketmine\level\sound\ClickSound;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+class ClickSound extends PluginBase{
+  
+  public function onEnable(){
+    $this->getServer()->getLogger()->info(Color::GREEN."Plugin is on")
+  }
+  
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    switch($cmd->getName()){
+      case 'sound':
+        $sender->getLevel()->addSound(new ClickSound($sender));
+    }
+  }
+}
