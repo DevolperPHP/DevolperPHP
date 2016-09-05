39c2f9e77051ae8e590e06a95e1d518be2225475
DevolperPHP <devolperphp@gmail.com>
2016-09-05 17:54:55 +0300

    Update Permission_Command.php

--- src/Permission/Permission_Command.php
+++ src/Permission/Permission_Command.php
@@ -7,3 +7,21 @@ use pocketmine\permission\Permission;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\utils\TextFormat as Color;
+
+class Permission_Command extends PluginBase{
+  
+  public function onEnable(){
+		$this->getServer()->getLogger()->info(Color::GREEN."Plugin is on");
+	}
+	
+	public function onCommand(CommandSender $sender, Command $cmd, $label array $args){
+	  switch($cmd->getName()){
+	    case 'cmd':
+	      if($sender->hasPermission("permission.cmd")){
+	        $sender->sendMessage("Hi");
+	      } else {
+	        $sender->sendMessage(Color::RED."You Not Have Permission");
+	      }
+	  }
+	}
+}
