342e28cd6ba6568abd1bb8c6c005b7cad84beaca
DevolperPHP <devolperphp@gmail.com>
2016-09-05 18:14:56 +0300

    Update Effect.php

--- src/Effect.php
+++ src/Effect.php
@@ -1,1 +1,26 @@
+<?php
 
+namespace Effect;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\utils\TextFormat as Color;
+
+class Effect extends PluginBase{
+  
+  public function onEnable(){
+    $this->getServer()->getLogger()->info(Color::GREEN."Plugin is on");
+  }
+  
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    switch($cmd->getName()){
+      case 'cmd':
+        $Effect = Effect::getEffect(2);  /* Efeect id */
+        $effect->setAmplifier(2);    /* Effect Power */
+        $effect->setVisible(true);
+        $effect->setDuration(10000000000);  /* Effect Time */
+        $sender->addEffect($effect);
+    }
+  }
+}
