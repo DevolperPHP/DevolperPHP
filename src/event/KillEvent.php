37be227c27281d4acd91e26d421a5ac7e280d8a3
DevolperPHP <devolperphp@gmail.com>
2016-09-05 16:19:15 +0300

    Update KillEvent.php

--- src/event/KillEvent.php
+++ src/event/KillEvent.php
@@ -1,1 +1,25 @@
+<?php
 
+namespace KillEvent;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+
+class KillEvent extends PluginBase implements Listener{
+  
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  
+  public function onKill(PlayerDeathEvent $event){
+    $cause = $event->getEntity()->getLastDamageCause();
+        if($cause instanceof EntityDamageByEntityEvent){
+            $killer = $cause->getDamager();
+				
+				$killer->setHealth(20);
+				$killer->setFood(20);
+      }
+  }
+}
