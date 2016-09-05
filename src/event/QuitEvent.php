ed90f856cd99af14837d166267c305a6190acd57
DevolperPHP <devolperphp@gmail.com>
2016-09-05 16:35:32 +0300

    Update QuitEvent.php

--- src/event/QuitEvent.php
+++ src/event/QuitEvent.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace QuitEvent;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\utils\TextFormat as Color;
+
+class QuitEvent extends PluginBase implements Listener{
+  
+  public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	
+	public function onQuit(PlayerQuitEvent $event){
+	  $player = $event->getPlayer();
+	  $name = $player->getName();
+	  
+	  $player->setQuitMessage(Color::YELLOW."$name Left The Server");
+	}
+}
