1b54d40ac076e5dd2be562a27e59c5c96a940c60
DevolperPHP <devolperphp@gmail.com>
2016-09-05 16:11:40 +0300

    Update KickEvent.php

--- src/event/KickEvent.php
+++ src/event/KickEvent.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace KickEvent;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerKickEvent;
+use pocketmine\utils\TextFormat as Color;
+
+class KickEvent extends PluginBase implements Listener{
+  
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  
+  public function onKick(PlayerKickEvent $event){
+    $player = $event->getPlayer();
+    $name = $event->getName();
+    
+    $player->setKickMessage(Color::DARK_RED."$name You Kick By Admin");
+  }
+}
