1290b4792492f902b8644ebd6016a9f7c1da663f
DevolperPHP <devolperphp@gmail.com>
2016-09-05 14:21:00 +0300

    Update EnchantParticle.php

--- src/Particle/EnchantParticle.php
+++ src/Particle/EnchantParticle.php
@@ -1,1 +1,23 @@
+<?php
 
+namespace EnchantParticle;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\level\particle\EnchantParticle;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\utils\TextFormat as Color;
+
+class AngryVillagerParticle extends PluginBase{
+  
+  public function onEnable(){
+    $this->getServer()->getLogger()->info(Color::GREEN."Plugin is on");
+  }
+  
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    switch($cmd->getName()){
+      case 'particle':
+        $sender->getLevel()->addParticle(new EnchantParticle($sender))
+    }
+  }
+}
