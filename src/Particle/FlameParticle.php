f8e81ea342528d3a082979593ac3a73d74c60add
DevolperPHP <devolperphp@gmail.com>
2016-09-05 14:26:50 +0300

    Update FlameParticle.php

--- src/Particle/FlameParticle.php
+++ src/Particle/FlameParticle.php
@@ -1,1 +1,22 @@
 
+<?php
+namespace FlameParticle;
+use pocketmine\plugin\PluginBase;
+use pocketmine\level\particle\FlameParticle;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\utils\TextFormat as Color;
+
+class FlameParticle extends PluginBase{
+  
+  public function onEnable(){
+    $this->getServer()->getLogger()->info(Color::GREEN."Plugin is on");
+  }
+  
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    switch($cmd->getName()){
+      case 'particle':
+        $sender->getLevel()->addParticle(new FlameParticle($sender));
+    }
+  }
+}
