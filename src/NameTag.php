42c0114e9c0d8ea03e6e883b1189a7da3d88b5bb
DevolperPHP <devolperphp@gmail.com>
2016-10-22 19:45:05 +0300

    Update NameTag.php

--- src/NameTag.php
+++ src/NameTag.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace NameTag;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\utils\TextFormat as Color;
+
+class NameTag extends PluginBase{
+  
+  public function onEnable(){
+    $this->getServer()->getLogger()->info(Color::GREEN."Plugin is on");
+  }
+  
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    switch($cmd->getName()){
+      case 'cmd':
+        $sender->setNameTag("[TAG]");
+    }
+  }
+}
